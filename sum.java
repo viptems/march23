0f773bde2457b328c6af2a379aa3f285fa4eeb16
SanjyotBelote <sanjyotbelote1@gmail.com>
2023-05-26 02:31:48 +0530

    sum.java modified

--- sum.java
+++ sum.java
@@ -2,6 +2,7 @@ class sum {
     public static void main() {
  
         System.out.print("In sum.java file");
-
+        System.out.print("In sum.java file");
+        System.out.print("In sum.java file");
     }
 }
